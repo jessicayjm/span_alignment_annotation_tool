44e615f44d07d94df29e665f6ae685b4c63b3a17
jessicayjm <jessicayjm@outlook.com>
2024-04-18 16:45:54 -0500

    finalize related files

--- sql/init.sql
+++ sql/init.sql
@@ -5,4 +5,10 @@ INSERT INTO annotators(fullname, username, email, password)
 VALUES ('Final Annotation', 'Final Annotation', 'admin@admin', '');
 -- DO NOT CHANGE --
 
+-- Add project admin info
+-- DO NOT set password here. You'll be able to set it on the website login as a first time user if you leave it blank.
+INSERT INTO annotators(fullname, username, email, password)
+VALUES ('Your full name', 'Your username', 'your email', '');
+-- Add project admin info
+
 -- add other initialization below
